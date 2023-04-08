9396eb8505ca1d92eb66dd5450693695ae335683
johannesgaebler <johannes.gaebler@gmail.com>
2023-04-08 16:23:21 +0200

    Added hdrp, added inital blockout

---
+++ Assets/_Project/Scripts/Game/GamePlay/LevelStaircaseLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStaircaseLogic : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> paths;
+
+
+    private void Awake()
+    {
+        paths.ForEach(p => p.SetActive(false));
+        paths.RandomElement().SetActive(true);
+    }
+}
