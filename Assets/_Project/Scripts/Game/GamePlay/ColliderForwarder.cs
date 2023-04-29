c121bccf9b755cc5d9a1a459eb20f6de7d8c7aa8
johannesgaebler <johannes.gaebler@gmail.com>
2023-04-29 11:35:36 +0200

    Added minimap and game scene manager

---
+++ Assets/_Project/Scripts/Game/GamePlay/ColliderForwarder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class ColliderForwarder : MonoBehaviour
+{
+    public event Action<Collider> OnTriggerEnterEvent;
+    public event Action<Collider> OnTriggerExitEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTriggerEnterEvent?.Invoke(other);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        OnTriggerExitEvent?.Invoke(other);
+    }
+}
